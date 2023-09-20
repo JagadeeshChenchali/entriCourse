fdf03316b419ea602bcce95d3d6168f0fb024b91
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-20 17:02:58 +0530

    Grade calculator

---
+++ DSA/September/dsa10.js
@@ -0,0 +1,18 @@
+/* 10)Grade Calculator
+Write a function calculateGrade that takes a student's score as input and prints their corresponding grade: "A" for scores 90 and above, "B" for scores 80-89, "C" for scores 70-79, "D" for scores 60-69, and "F" for scores below 60.
+*/
+
+function calculateGrade(score) {
+    if (score >= 90) {
+        console.log("A");
+    } else if (score >= 80) {
+        console.log("B");
+    } else if (score >= 70) {
+        console.log("C");
+    } else if (score >= 60) {
+        console.log("D");
+    } else {
+        console.log("F");
+    }
+}
+console.log(calculateGrade(91));
