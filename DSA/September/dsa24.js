45a3e61ac0dfb21a0d686fcd052e3e79b83b7758
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-20 18:47:00 +0530

    Multiplication Table

---
+++ DSA/September/dsa24.js
@@ -0,0 +1,16 @@
+/*
+**4. Print Multiplication Table:**
+Write a program to print the multiplication table of a given number `n` using a `for` loop.
+*/
+
+const n = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${n} x ${i} = ${n * i}`);
+}
+
+//n=3 ,i
+//n*i= n*i
+//2*1==2
+//n*i=4
+//n*i=6
+//n*i=8
