5554fef665d199ef2831f5c6470a9de29f1c845f
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-20 18:28:07 +0530

    Factorial of a Number

---
+++ DSA/September/dsa22.js
@@ -0,0 +1,14 @@
+/*
+**2. Factorial of a Number:**
+Write a program to calculate the factorial of a given number `n` using a `for` loop.
+*/
+
+const n = 5;
+let factorial = 1;
+for (let i = 1; i <= n; i++) {
+  factorial *= i;
+}
+//1*2*3*4*5=120
+//for (let i = 5; i <= n; i--)
+//5*4*3*2*1=120
+console.log(factorial); // 120
