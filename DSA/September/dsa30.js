489973084235ab8cc1c5730b5dca2be1cb93aa77
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-21 08:24:45 +0530

    reverse an array

---
+++ DSA/September/dsa30.js
@@ -0,0 +1,11 @@
+/*
+**10. Reverse an Array:**
+Write a program to reverse the elements of an array using a `for` loop.
+*/
+
+const inputArray = [1, 2, 3, 4, 5];
+const reversedArray = [];    
+for (let i = inputArray.length - 1; i >= 0; i--) {
+  reversedArray.push(inputArray[i]);
+}
+console.log(reversedArray);
