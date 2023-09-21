06436f504b9dd1349d05f7190d4df29ea5958bc0
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-21 10:36:56 +0530

    using a function to reverse a string

--- DSA/September/dsa31.js
+++ DSA/September/dsa31.js
@@ -1,21 +1,13 @@
 /*
-**Question 1: Reverse a String**
-
 Write a function `reverseString` that takes a string as input and returns the reverse of that string.
 */
 
 function reverseString(str) {
-
-    // empty string
-    let newString = "";
-    for (let i = str.length - 1; i >= 0; i--) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
         newString += str[i];
     }
     return newString;
 }
-
-// take input from the user
-const string = "hello";
-
-const result = reverseString(string);
+const result=reverseString('hello');
 console.log(result);
