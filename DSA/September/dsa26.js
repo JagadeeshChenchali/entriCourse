0a8fddad2449208c7243e37cca955b968891b80f
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-25 15:55:59 +0530

    checking prime number

--- DSA/September/dsa26.js
+++ DSA/September/dsa26.js
@@ -3,7 +3,7 @@
 Write a program to check if a given number `n` is prime using a `for` loop.
 */
 
-const n = 7;
+/*const n = 7;
 let isPrime = true;
 for (let i = 2; i <= Math.sqrt(n); i++) {
   if (n % i === 0) {
@@ -11,4 +11,20 @@ for (let i = 2; i <= Math.sqrt(n); i++) {
     break;
   }
 }
-console.log(isPrime);
+console.log(isPrime);*/
+
+function isPrime(number){
+  let count = 0;
+  for(let i = 1;i<=number;i++)    {
+     if(number%i == 0){
+        count = count + 1;
+     }
+  }
+  if(count == 2){
+     return true;
+  }
+  else{
+     return false;
+  }
+}
+console.log(isPrime(10));
